5175dd60ae4ab6737554da99591d5ffce4881191
Marco Herrn <marco@mherrn.de>
2024-03-22 21:22:30 +0100

    Provide a few more Coat focused tests

--- coat/src/jmh/java/io/github/joeljeremy7/java/config/lib/benchmarks/Config.java
+++ coat/src/jmh/java/io/github/joeljeremy7/java/config/lib/benchmarks/Config.java
@@ -1,11 +1,15 @@
 package io.github.joeljeremy7.java.config.lib.benchmarks;
 
 import de.poiu.coat.annotation.Coat;
+import java.time.LocalDate;
+import java.util.List;
 
 
 @Coat.Config
-public interface Config {
-  public String test1();
+public interface Config extends SimpleConfig {
 
-  public int testInt1();
+  public List<LocalDate> listOfDates();
+
+  @Coat.Embedded
+  public MqttConfig mqtt();
 }
